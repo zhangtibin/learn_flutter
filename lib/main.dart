ba6d5e33ce4cc9b82a784fd5c498ce298b95fe9f
tibin.zhang <tibin.zhang@huifu.com>
2020-04-09 00:32:33 +0800

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: '首页'),
     );
   }
 }
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              '测试什么呢:',
             ),
             Text(
               '$_counter',
